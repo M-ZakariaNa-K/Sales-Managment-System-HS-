fd28ce6d189b66d39e89d8dadff2dc814ee3674b
Zakaria1003 <114285827+Zakaria1003@users.noreply.github.com>
2024-01-28 09:27:03 -0700

    Adding all Folder

--- lib/Core/Constants/theme.dart
+++ lib/Core/Constants/theme.dart
@@ -4,5 +4,5 @@ class ThemeColors {
   static const primary = Color(0xFFE61B23);
   static const primaryTextColor = Color(0xFF000000);
   static const secondaryTextColor = Color(0xFFffffff);
-  static const secondary = Color(0xFFC29A46);
+  static const secondary = Color(0xFFC29A46);//qq
 }
