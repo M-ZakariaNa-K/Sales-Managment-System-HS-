6607a82b8973d7eb8b73af20f20b8705c06bfc2a
Zakaria1003 <114285827+Zakaria1003@users.noreply.github.com>
2024-01-28 09:18:31 -0700

    Adding all Folders

---
+++ lib/Core/Constants/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const primary = Color(0xFFE61B23);
+  static const primaryTextColor = Color(0xFF000000);
+  static const secondaryTextColor = Color(0xFFffffff);
+  static const secondary = Color(0xFFC29A46);
+}
