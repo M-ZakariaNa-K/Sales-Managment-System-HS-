6cb49abeb288eaa71edb7ec1352fbb69b92ae188
Zakaria1003 <114285827+Zakaria1003@users.noreply.github.com>
2024-02-07 05:46:58 -0700

    change colors and add assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:sales_management_system/Core/Constants/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sales_management_system/Views/home/home.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-// NOTICE: Do not play anything else  
+// NOTICE: Do not play anything else
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,7 +24,7 @@ class MyApp extends StatelessWidget {
       title: 'Sales Management System App',
       // NOTE (From Zakaria): everyone when creating your task page
       // put the home page as your main working screen
-      // home:,
+      home: HomePage(),
       theme: ThemeData(
         useMaterial3: true,
         textTheme: GoogleFonts.robotoTextTheme(),
@@ -30,19 +32,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
           seedColor: ThemeColors.primary,
           secondary: ThemeColors.secondary,
-          outline: const Color.fromARGB(198, 0, 51, 102),
+          outline:  ThemeColors.secondary,
         ),
         inputDecorationTheme: const InputDecorationTheme(
           border: outlineInputBorder,
           enabledBorder: outlineInputBorder,
           focusedBorder: outlineInputBorder,
-          prefixIconColor: Color(0xFF003366),
+          prefixIconColor: ThemeColors.secondary,
           outlineBorder: BorderSide(
             width: .8,
-            color: Color.fromARGB(198, 0, 51, 102),
+            color: ThemeColors.secondary,
           ),
           hintStyle: TextStyle(
-            color: Color.fromARGB(198, 0, 51, 102),
+            color: Colors.grey,
           ),
         ),
       ),
